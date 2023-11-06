21385dc2a5ee718d150afd0076f8a6c6c6759ca9
pqxglobal <l.r.slegt@gmail.com>
2023-11-06 18:17:18 +0100

    🚧 1:26:49 API Routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 'use client'
 
+import Image from 'next/image'
+import factory from '../../public/next.svg'
+
 export default function Home() {
-  return <div>Main page</div>
+  return <div className='w-[800px]'>
+    <Image quality={50} src={factory} alt="factory" />
+  </div>
 }
