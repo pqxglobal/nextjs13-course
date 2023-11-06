5f288026aa627a89466628fef34a7f34d76b689c
pqxglobal <l.r.slegt@gmail.com>
2023-11-06 19:21:36 +0100

    🚧 1:36:46 fonts

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,8 @@
+import data from '@/data.json'
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  return NextResponse.json({
+    data,
+  })
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,37 @@
 'use client'
 
-import Image from 'next/image'
-import factory from '../../public/next.svg'
+import { useState } from 'react'
 
 export default function Home() {
-  return <div className='w-[800px]'>
-    <Image quality={50} src={factory} alt="factory" />
-  </div>
+  const [loading, setLoading] = useState(false)
+  const fetchDataFromApi = async () => {
+    try {
+      setLoading(true)
+      const response = await fetch('/api/users', {
+        headers: {
+          Accept: 'application/json',
+          method: 'GET',
+        },
+      })
+      if (response) {
+        const data = await response.json()
+        console.log(data)
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <div>
+      <button
+        className="px-4 py-2 font-bold text-white bg-blue-500 cursor-pointer"
+        onClick={() => fetchDataFromApi()}
+      >
+        {loading ? 'Loading...' : 'Call my API'}
+      </button>
+    </div>
+  )
 }
