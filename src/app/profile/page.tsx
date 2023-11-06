87ef491d79cc397818a73bad71ed2f86d0181cd1
pqxglobal <l.r.slegt@gmail.com>
2023-11-06 07:28:41 +0100

    56:46 components

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 
 export default function Profile() {
@@ -11,11 +12,23 @@ export default function Profile() {
     <div>
       <h1>my list of profiles</h1>
       <div>
-        <ul className="list-disc list-outside ms-8">
-          <li onClick={() => router.push('/profile/1')}>Profile 1</li>
-          <li onClick={() => router.push('/profile/2')}>Profile 2</li>
-          <li onClick={() => router.push('/profile/3')}>Profile 3</li>
-        </ul>
+        <Link href="/profile/1">Profile 1</Link>
+        <Link href="/profile/2">Profile 2</Link>
+        <Link
+          href={{
+            pathname: '/profile/3',
+            query: {
+              userName: 'Guillaume',
+              userId: 12,
+              person: JSON.stringify({
+                age: 18,
+              }),
+            },
+          }}
+        >
+          <div>A big square where I got some text</div>
+        </Link>
+        <a href="https://codewithguillaume.com">Codewithguillaume&apos;s school online</a>
       </div>
     </div>
   )
