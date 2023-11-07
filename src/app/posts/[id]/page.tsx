b8406d422d47b0ba58f43c8c46dc4e862208c95d
pqxglobal <l.r.slegt@gmail.com>
2023-11-07 07:27:10 +0100

    🚧 2:26:24 cache explained

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,4 +1,21 @@
-export default function PostId({ params, searchParams }: any) {
-  console.log(searchParams)
-  return <main>Post {searchParams.new}</main>
+async function getPostById(postId: string) {
+  const response = await fetch(`http://localhost:3000/api/posts/${postId}`, {
+    method: 'GET',
+    next: {
+      revalidate: 5000,
+    },
+  })
+
+  return response.json()
+}
+
+export default async function PostID({ params }: any) {
+  const { post } = await getPostById(params.id)
+
+  return (
+    <main>
+      <h1>{post.title}</h1>
+      <p>{post.description}</p>
+    </main>
+  )
 }
